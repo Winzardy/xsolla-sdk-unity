8f81e3babbf3f763a277f36accb661e0e28eabfa
Vladislav Evdokimov <vladisevdokim@gmail.com>
2026-04-14 21:34:16 +0300

    [!] Remove linked package framework from main target on post process

--- Editor/XsollaSDKXcodePostProcess.cs
+++ Editor/XsollaSDKXcodePostProcess.cs
@@ -49,7 +49,6 @@ namespace Xsolla.SDK.Editor
             string frameworkTarget = pbxProject.GetUnityFrameworkTargetGuid();
 
             string packageGuid = pbxProject.AddRemotePackageReferenceAtVersionUpToNextMinor(PackageUrl, PackageVersion);
-            pbxProject.AddRemotePackageFrameworkToProject(mainTarget, PackageProduct, packageGuid, false);
             pbxProject.AddRemotePackageFrameworkToProject(frameworkTarget, PackageProduct, packageGuid, false);
 
             pbxProject.WriteToFile(projectPath);
